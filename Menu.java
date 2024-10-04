3291eb36d1570ad913c1737d44b1bff31133e8c5
Adriano047 <106997880+Adriano047@users.noreply.github.com>
2024-10-04 10:49:47 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -5,10 +5,11 @@ public class Menu {
     Alimentos Alimentos = new Alimentos();    
     Refeição Refeição = new Refeição();    
     Cardapio Cardapio = new Cardapio();    
+    Semanal Menu = new Semanal();    
     void Iniciar() {
         while (true) {
-           String Opcoes[] = {"Add", "Criar", "Menu", "Semanal"};
-            Integer Valor = JOptionPane.showOptionDialog(null, "- Adicionar Alimento\n- Criar Refeição\n- Montar Menu\n- Fazer Cardapio Semanal", "Restaurante LeDelicious", 0, 3, null, Opcoes, Opcoes[0]);
+           String Opcoes[] = {"Add", "Criar", "Menu", "Semanal", "Verificar"};
+            Integer Valor = JOptionPane.showOptionDialog(null, "- Adicionar Alimento\n- Criar Refeição\n- Montar Menu\n- Fazer Cardapio Semanal\n - Vericar Estado Atual do cardapio", "Restaurante LeDelicious", 0, 3, null, Opcoes, Opcoes[0]);
             if(Valor.equals(0)) {
                 Alimentos.Add();
             }else if(Valor.equals(1)) {
@@ -16,8 +17,10 @@ public class Menu {
             }else if(Valor.equals(2)) {
                 Cardapio.Montar();
             }else if(Valor.equals(3)) {
-            
-            }else{
+                Menu.DaSemana();
+            }else if(Valor.equals(4)) {
+                Menu.ResultadoFinal();
+            }else {
                 break;
             } 
         } 
