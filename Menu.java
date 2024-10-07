e4349536fa877a89c8731d021c011a21ecdf8194
Adriano047 <106997880+Adriano047@users.noreply.github.com>
2024-10-07 10:49:37 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -2,27 +2,17 @@
 import javax.swing.JOptionPane;
 
 public class Menu {
-    Alimentos Alimentos = new Alimentos();    
-    Refeição Refeição = new Refeição();    
     Cardapio Cardapio = new Cardapio();
-    Semanal Menu = new Semanal();    
-    void Iniciar() {
+     void Iniciar() {
         while (true) {
-           String Opcoes[] = {"Add", "Criar", "Menu", "Semanal", "Verificar"};
-            Integer Valor = JOptionPane.showOptionDialog(null, "- Adicionar Alimento\n- Criar Refeição\n- Montar Menu\n- Fazer Cardapio Semanal\n - Vericar Estado Atual do cardapio", "Restaurante LeDelicious", 0, 3, null, Opcoes, Opcoes[0]);
+           String Opcoes[] = {"Add", "Verificar"};
+            Integer Valor = JOptionPane.showOptionDialog(null, "Opções Disponiveis:\n- Adicionar ao cardapio\n - Vericar Estado Atual do cardapio", "Restaurante LeDelicious", 0, 3, null, Opcoes, Opcoes[0]);
             if(Valor.equals(0)) {
-                Alimentos.Add();
+                Cardapio.Add();
             }else if(Valor.equals(1)) {
-               Refeição.Criar();
-            }else if(Valor.equals(2)) {
-                Cardapio.Montar();
-            }else if(Valor.equals(3)) {
-                Menu.DaSemana();
-            }else if(Valor.equals(4)) {
-                JOptionPane.showMessageDialog(null, Menu.VerificarProgressao());
-                
+                JOptionPane.showMessageDialog(null, Cardapio.Vericar());
             }else {
-                System.out.println(Menu.ResultadoFinal());
+                System.out.println(Cardapio.ResultadoFinal());
                 break;
             } 
         } 
